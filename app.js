b3205858b992415bafbbba2882a6d809ffae0503
Ujjawl <srivastavujjawal123@gmail.com>
2020-09-27 08:13:13 +0530

    'deploy'

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require("express")
+const config = require('dotenv').config()
+const app = express()
+app.get("/",(req,res)=>res.send("Hello"))
+app.listen(process.env.PORT,()=>console.log("Server Running"))
